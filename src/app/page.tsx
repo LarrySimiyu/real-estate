d75a310ddd32c18d71a8aa2e4c89b7c7073e618c
Larry Simiyu <larry.simiyu@gmail.com>
2023-05-26 14:16:12 -0700

    section 1 finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 import { ArrowRight } from "react-feather";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center bg-black text-white">
-      <div className=" z-10 w-full  items-center justify-between text-sm lg:flex md:h-[70px] md:px-10">
+    <main className="flex min-h-screen flex-col items-center bg-black text-white md:px-10">
+      <div className=" z-10 w-full  items-center justify-between text-sm lg:flex md:h-[70px]">
         <div className="font-bold text-[60px]">PRI.</div>
         <button className="border rounded-3xl  md:w-[10%] md:h-12 hover:text-black hover:bg-white  text-[18px] flex justify-center items-center">
           <div className="mr-2">Contact</div>
@@ -23,6 +23,19 @@ export default function Home() {
             <div className="mr-2">Contact</div>
             <ArrowRight size={24} />
           </button>
+        </div>
+      </div>
+      <div className=" md:w-full md:flex md:h-[300px]">
+        <div className=" md:w-[100%]">
+          <h1>About</h1>
+        </div>
+        <div className="  text-[30px] pr-20">
+          Vivamus non massa purus. Proin vestibulum lobortis libero. Sed enim
+          velit, iaculis vel viverra ut, laoreet in tortor. Vestibulum ultricies
+          turpis non erat faucibus interdum. Fusce venenatis erat orci, in
+          sodales justo facilisis eu. Suspendisse id augue libero. Aliquam
+          aliquam ligula sem, quis condimentum mi vestibulum eget. Ut sit amet
+          vestibulum lacus.
         </div>
       </div>
     </main>
