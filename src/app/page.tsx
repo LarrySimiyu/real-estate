884e772d7085dd108a30d562f2e17ebee3e1f556
Larry Simiyu <larry.simiyu@gmail.com>
2023-05-26 11:18:27 -0700

    contact button added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
-
+import { ArrowRight } from "react-feather";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-black text-white">
-      <div className="border z-10 w-full  items-center justify-between font-mono text-sm lg:flex md:h-[70px] px-3">
+      <div className="border z-10 w-full  items-center justify-between font-mono text-sm lg:flex md:h-[70px] px-5">
         <div className="font-bold text-[60px]">PRI.</div>
-        <button className="border rounded-2xl  md:w-[15%] md:h-10 text-black bg-white text-[18px]">
-          Contact
+        <button className="border rounded-3xl  md:w-[10%] md:h-12 hover:text-black hover:bg-white  text-[18px] flex justify-center items-center">
+          <div className="mr-2">Contact</div>
+          <ArrowRight size={24} />
         </button>
       </div>
-      <div className="border w-full h-[100vh]">
+      <div className="border w-full h-[100vh] px-5">
         <div className="border md:w-1/2">
           <div className="md:text-[30px] font-medium border">
             Invest in real estate
