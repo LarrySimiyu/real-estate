e2c19593603f531a3fb064893d92eb789d4b4345
Larry Simiyu <larry.simiyu@gmail.com>
2023-07-10 10:13:38 -0700

    image added

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -5,10 +5,24 @@ const Properties = () => {
   return (
     <div className="flex flex-col min-h-screen bg-black text-white md:px-20 pb-5">
       <div className="mt-5">
-        <div>Properties</div>
+        <div className="font-bold text-[30px]">Properties</div>
       </div>
-      <div className="mt-5 flex flex-wrap justify-between">
-        <Image
+      <div className="mt-5 flex flex-wrap justify-between ">
+        <div className="flex flex-col cursor-pointer">
+          <Image
+            src={house}
+            alt="Header"
+            className="h-[160px] w-[260px] rounded-md mb-2 hover:border-2 hover:border-white"
+          />
+          <div className="font-bold text-[25px] flex items-center justify-between w-full">
+            $40,000,000{" "}
+            <span className="text-gray-500 text-[12px]">+ Buyer Fee</span>
+          </div>
+          <div className="font-bold">216 Units</div>
+          <div className="font-bold">Fort Worth, TX</div>
+        </div>
+
+        {/* <Image
           src={house}
           alt="Header"
           className="h-[160px] w-[260px] rounded-md mb-10"
@@ -32,12 +46,7 @@ const Properties = () => {
           src={house}
           alt="Header"
           className="h-[160px] w-[260px] rounded-md mb-10"
-        />
-        <Image
-          src={house}
-          alt="Header"
-          className="h-[160px] w-[260px] rounded-md mb-10"
-        />
+        /> */}
 
         <div className="h-[160px] w-[260px] mb-10"></div>
         <div className="h-[160px] w-[260px] mb-10"></div>
